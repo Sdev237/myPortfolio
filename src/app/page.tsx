f03379d8382069361f20b326441be975aa809c77
Sdev237 <sdevsap@gmail.com>
2024-11-28 22:17:18 -0500

    factorisation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,29 @@
 "use client";
 
-import React, { useState } from 'react';
-import Navbar from '../components/Navbar';
-import Accueil from '@/components/Accueil';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Footer } from '@/components/Footer';
+import React, { useState } from "react";
+import Navbar from "../components/navbar/Navbar";
+import Accueil from "@/components/accueil/Accueil";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Footer } from "@/components/footer/Footer";
 export default function Home() {
+  const [theme, setTheme] = useState("dark");
+  const [langue, setLangue] = useState("Fr");
 
-  const [theme, setTheme] = useState('dark');
-  const [langue, setLangue] = useState('Fr');
-  
-  const [langueProfil, setLangueProfil] = useState('Fr');
+  const [langueProfil, setLangueProfil] = useState("Fr");
 
   const toggleTheme = () => {
-    setTheme(theme === 'light' ? 'dark' : 'light');
+    setTheme(theme === "light" ? "dark" : "light");
   };
 
   const toggleLangue = () => {
-    setLangue(langue === 'Fr' ? 'En' : 'Fr');
-    
-    setLangueProfil(langueProfil === 'Fr' ? 'En' : 'Fr');
+    setLangue(langue === "Fr" ? "En" : "Fr");
+
+    setLangueProfil(langueProfil === "Fr" ? "En" : "Fr");
   };
 
   return (
-    <main  className={`app ${theme} ${langue} ${langueProfil}`}>
-      <Navbar toggleTheme={toggleTheme} toggleLangue={toggleLangue} />     
+    <main className={`app ${theme} ${langue} ${langueProfil}`}>
+      <Navbar toggleTheme={toggleTheme} toggleLangue={toggleLangue} />
       <Accueil />
     </main>
   );
