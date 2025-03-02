482a2653255e555ea1c1596d7f94186c1ccb961c
Sdev237 <sdevsap@gmail.com>
2025-03-01 22:53:59 -0500

    add projet files

---
+++ src/components/Projets/Projets.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Projets.scss'
+
+export const Projets = () => {
+  return (
+    <div>
+      <h1>Projets</h1>
+      <p>Cette section présente les différents projets que j'ai réalisés.</p>
+      <ul>
+        <li>Projet 1 : Développement d'un site web pour une entreprise</li>
+        <li>Projet 2 : Création d'une application mobile pour la gestion des tâches</li>
+        <li>Projet 3 : Intégration d'un système de paiement sécurisé pour un e-commerce</li>
+      </ul>
+    </div>
+  )
+}
