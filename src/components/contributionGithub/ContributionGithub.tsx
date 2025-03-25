b135dd20e66b193c13c4c5b79996f9f08aa063ef
Sdev237 <sdevsap@gmail.com>
2025-03-24 23:33:55 -0400

    update Projet and ContributionGithub file

--- src/components/contributionGithub/ContributionGithub.tsx
+++ src/components/contributionGithub/ContributionGithub.tsx
@@ -1,15 +1,3 @@
-{/*import React from 'react';
-import GitHubContributions from 'react-github-contributions';
-
-const ContributionGithub = () => {
-  return (
-    <div>
-      <aside className='' style={{border: '1px  #333', borderRadius: '0.4rem'}}>
-        <GitHubContributions username="Sdev237" />
-      </aside>
-    </div>
-  );
-};
 
 export default ContributionGithub;*/}
 
