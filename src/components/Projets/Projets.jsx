259476a373b2fb80fa3427e20a2774e1c5ed6e2c
Sdev237 <sdevsap@gmail.com>
2025-04-30 17:23:42 -0400

    update

--- src/components/Projets/Projets.jsx
+++ src/components/Projets/Projets.jsx
@@ -6,14 +6,6 @@ export const Projets = () => {
     <div>
       <h1>Projets</h1>
       <p>Cette section présente les différents projets que j'ai réalisés.</p>
-      <ul>
-        <li>Projet 1 : Développement d'un site</li>
-        <li>Projet 2 : Création d'une application mobile pour la planificastion des projets</li>
-        <li>Projet 3 : Intégration d'un système de paiement avec strip</li>
-        <li>Projet 1 : Développement d'un site</li>
-        <li>Projet 2 : Création d'une application mobile pour la planificastion des projets</li>
-        <li>Projet 3 : Intégration d'un système de paiement avec strip</li>
-      </ul>
-    </div>
+     
   );
 };
