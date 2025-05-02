a7501edf824b44a5d558b31a024ef60f0138672d
Sdev237 <sdevsap@gmail.com>
2025-05-02 07:59:50 -0400

    update

--- src/components/Projets/Projets.jsx
+++ src/components/Projets/Projets.jsx
@@ -3,9 +3,6 @@ import "./Projets.scss";
 
 export const Projets = () => {
   return (
-    <div>
-      <h1>Projets</h1>
-      <p>Cette section présente les différents projets que j'ai réalisés.</p>
-     
+   
   );
 };
