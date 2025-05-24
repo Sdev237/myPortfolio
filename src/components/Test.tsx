3a75d8351a6ca83d8e064d2aa58556d36ba48ec2
Sdev237 <sdevsap@gmail.com>
2025-05-23 22:45:37 -0400

    modified:   src/components/Test.tsx

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -7,27 +7,7 @@ const Test = () => {
       <h2>Contribution activity</h2>
       <div className="month">October 2024</div>
       <div className="timeline">
-        <div className="event">
-          <div className="icon">🔄</div>
-          <div className="details">
-            <h3>Created 12 commits in 2 repositories</h3>
-            <ul>
-              <li>Sdev237/Landing-page - 8 commits</li>
-              <li>Sdev237/myPortfolio - 4 commits</li>
-            </ul>
-          </div>
-        </div>
-        <div className="event">
-          <div className="icon">📁</div>
-          <div className="details">
-            <h3>Created 2 repositories</h3>
-            <ul>
-              <li>Sdev237/Landing-page</li>
-              <li>Sdev237/myPortfolio</li>
-              <li>Sdev237/Landing-page</li>
-              <li>Sdev237/myPortfolio</li>
-            </ul>
-          </div>
+        
         </div>
       </div>
     </div>
