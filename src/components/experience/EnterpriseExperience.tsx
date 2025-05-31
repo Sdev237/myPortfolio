b8d604d951e5b4accf733802fd4ba9e7f812a16b
Sdev237 <sdevsap@gmail.com>
2025-05-31 07:20:21 -0400

    modified:   src/components/accueil/Accueil.scss         modified:   src/components/experience/EnterpriseExperience.tsx

--- src/components/experience/EnterpriseExperience.tsx
+++ src/components/experience/EnterpriseExperience.tsx
@@ -3,7 +3,18 @@ import "./EnterpriseExperience.scss";
 
 export const EnterpriseExperience = () => {
   const company = [
-   
+    {
+      name: "Orange Digital Center",
+      logo: "https://c.woopic.com/logo-orange.png",
+      poste: "Programmer Analyst",
+      date: "2022-2023",
+    },
+    {
+      name: "Hugo Company LTD",
+      logo: "../app/fonts/hugo.png",
+      poste: "IT Infrastructure Manager",
+      date: "2020-2022",
+    },
     {
       name: "CENADI",
       logo: "https://i0.wp.com/www.cenadi.cm/wp-content/uploads/2020/09/armoiries_logo_cenadi.png?fit=180%2C80&amp;ssl=1",
