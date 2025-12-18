af2b8cfecb90053f2d1f26889ae442366780c818
Sdev237 <sdevsap@gmail.com>
2025-12-18 17:44:47 -0500

    update ContributionGithub SASS and tsx  File

--- src/components/contributionGithub/ContributionGithub.tsx
+++ src/components/contributionGithub/ContributionGithub.tsx
@@ -1,4 +1,19 @@
+{
+  /*import React from 'react';
+import GitHubContributions from 'react-github-contributions';
 
+const ContributionGithub = () => {
+  return (
+    <div>
+      <aside className='' style={{border: '1px  #333', borderRadius: '0.4rem'}}>
+        <GitHubContributions username="Sdev237" />
+      </aside>
+    </div>
+  );
+};
+
+export default ContributionGithub;*/
+}
 
 import React, { useState } from "react";
 import "./ContributionGithub.scss";
