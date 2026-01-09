b93fcfd60910b39d49e4c9682f6a8718d0eb4282
Sdev237 <sdevsap@gmail.com>
2026-01-09 06:44:55 -0500

    update accueil.tsx, contributionGithub.tsx, Footer.scss  File

--- src/components/contributionGithub/ContributionGithub.tsx
+++ src/components/contributionGithub/ContributionGithub.tsx
@@ -130,20 +130,7 @@ const ContributionGithub: React.FC = () => {
                 </div>
               ))}
             </div>
-            <div className="squares-container">
-              {allDays.map((date) => {
-                const contribution = contributions.find(
-                  (c) => c.date === date
-                ) || { date, count: 0 };
-                return (
-                  <div
-                    key={date}
-                    className={`contribution-square level-${contribution.count}`}
-                    title={`${date}: ${contribution.count} contributions`}
-                  />
-                );
-              })}
-            </div>
+      
           </div>
           <div className="graph-footer">
             <span className="learn-more">
