314d201248d29b054710e29969d3fdee34b85c8a
Sdev237 <sdevsap@gmail.com>
2026-01-29 21:53:33 -0500

    update projets.scss, contributionGithub.tsx, Footer.scss  File

--- src/components/contributionGithub/ContributionGithub.tsx
+++ src/components/contributionGithub/ContributionGithub.tsx
@@ -160,15 +160,7 @@ const ContributionGithub: React.FC = () => {
         </div>
         <div className="yearSelector">
           {years.map((year) => (
-            <button
-              key={year}
-              className={`yearSelectorButton ${
-                year === activeYear ? "active" : ""
-              }`}
-              onClick={() => setActiveYear(year)}
-            >
-              {year}
-            </button>
+          
           ))}
         </div>
       </div>
